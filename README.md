3826a6aec4f173ffb603a3062bcf413d9f737ad2
Vladyslav <56305121+vladrevers@users.noreply.github.com>
2024-05-29 18:27:50 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# autopif
+
+English | [Українська](./README_UK.md) | [Русский](./README_RU.md)
+
+Lightweight fork of [daboynb/playcurl](https://github.com/daboynb/PlayIntegrityNEXT/tree/main/playcurl) that performs a single task: Every 25 minutes, it downloads the [autojson/pif.json](https://github.com/daboynb/autojson/blob/main/pif.json) file and compares your current `custom.pif.json` or `pif.json` file with the downloaded version. If your file differs or is missing, it replaces it, stops the GMS/Wallet services, and clears their cache. If the files are identical or an error occurs (e.g., no internet connection or failed to download the file), it does nothing in that iteration/attempt. It is intended to be used with (in my preference) the [osm0sis/PlayIntegrityFork](https://github.com/osm0sis/PlayIntegrityFork) module, or alternatively with [chiteroman/PlayIntegrityFix](https://github.com/chiteroman/PlayIntegrityFix).
+
+The 25-minute interval can be changed, please see the [Preferences](#preferences) and [Notes](#notes) sections.
+
+## Installation
+
+1. If the [osm0sis/PlayIntegrityFork](https://github.com/osm0sis/PlayIntegrityFork/releases/latest) or [chiteroman/PlayIntegrityFix](https://github.com/chiteroman/PlayIntegrityFix/releases/latest) module is not installed yet, install it (no need to reboot your device yet, it will just be faster).
+
+2. Install this [autopif](https://github.com/vladrevers/autopif/releases/latest) module.
+
+3. Reboot your device. If you want to see the results immediately, ensure your internet connection is enabled **before** the reboot.
+
+## Preferences
+
+Within the installed autopif module directory (`/data/adb/modules/autopif`) or inside the module's ZIP file before installation, you can find or create the following files for customization:
+
+- `minutes.txt` (contains only an integer value, specifying the interval in minutes for checking updates, default is 25)
+- `log_file_on` (if this file exists, a log will be written to `/storage/emulated/0/autopif_log.txt`. Present by default, remove this file if you don't need logging)
+- `log_console_on` (if this file exists, the log will be printed to the console, useful for monitoring progress when manually running in busybox ash. Not present by default, mainly added for testing during development)
+
+## Notes
+
+After installation, I recommend checking the `autopif_log.txt` file in your Internal Storage after 12-24 hours of habitual device usage. I've noticed that on my device, running Android 14, the script is paused by Magisk or Android itself when the screen is off for an extended period and no background tasks are running (e.g., music playback). The script resumes automatically when the screen is turned on. If you encounter a similar situation, it might make sense to decrease the check interval from 25 minutes to, for example, 20 minutes. However, 25 minutes should generally be sufficient. Conversely, if the script runs exactly on the set interval (e.g., on an older version of Android), you might want to increase the interval from 25 minutes to, for example, 45 minutes. Setting a very short interval, especially less than 10 minutes, is not highly recommended because the [autojson/pif.json](https://github.com/daboynb/autojson/blob/main/pif.json) file on the server is updated once per hour. After monitoring, you can disable logging and delete the log file. For instructions on changing the check interval and disabling logging, see the [Preferences](#preferences) section.
---
+++ module/service.sh
@@ -0,0 +1,150 @@
+MODPATH="${0%/*}"
+PIF_FILE_PATH=""
+
+log() {
+    log_file_path="/storage/emulated/0/autopif_log.txt"
+    if [ -f "$MODPATH/log_file_on" ] && touch "$log_file_path" 2>/dev/null; then
+        echo "$(date "+%Y-%m-%d_%H:%M:%S"): $1" >> "$log_file_path"
+    fi
+
+    if [ -f "$MODPATH/log_console_on" ]; then
+        echo "$1"
+    fi
+}
+
+set_pif_file_path() {
+    chiteroman_pif_file_path="/data/adb/pif.json"
+    osm0sis_pif_file_path="/data/adb/modules/playintegrityfix/custom.pif.json"
+    module_prop_path="/data/adb/modules/playintegrityfix/module.prop"
+
+    if grep -q 'author=osm0sis' "$module_prop_path" 2>/dev/null; then
+        PIF_FILE_PATH="$osm0sis_pif_file_path"
+    elif grep -q 'author=chiteroman' "$module_prop_path" 2>/dev/null; then
+        PIF_FILE_PATH="$chiteroman_pif_file_path"
+    elif [ -e "$osm0sis_pif_file_path" ]; then
+        PIF_FILE_PATH="$osm0sis_pif_file_path"
+    elif [ -e "$chiteroman_pif_file_path" ]; then
+        PIF_FILE_PATH="$chiteroman_pif_file_path"
+    else
+        PIF_FILE_PATH="$chiteroman_pif_file_path"
+        log "No specific PIF file or author found, using default path."
+    fi
+
+    log "Path to PIF file: $PIF_FILE_PATH"
+}
+
+check_network_reachable() {
+    attempt_count=0
+    max_attempts=8
+    success_pinged_series=0
+
+    while [ $attempt_count -lt $max_attempts ]; do
+        if ping -c1 -W5 "connectivitycheck.gstatic.com" > /dev/null 2>&1; then
+            success_pinged_series=$((success_pinged_series + 1))
+            if [ $success_pinged_series -ge 3 ]; then
+                log "Network is reachable."
+                return 0
+            fi
+        else
+            success_pinged_series=0
+        fi
+        attempt_count=$((attempt_count + 1))
+        sleep 0.5
+    done
+
+    log "Network is not reachable."
+    return 1
+}
+
+update_pif_if_needed() {
+    remote_pif_file_path="/data/adb/remote_pif.json"
+    log "Downloading remote PIF file."
+
+    if ! wget --no-check-certificate -q -O "$remote_pif_file_path" "https://raw.githubusercontent.com/daboynb/autojson/main/pif.json"; then
+        log "Failed to download remote PIF file."
+        return 1
+    fi
+
+    if grep -q '"FINGERPRINT": "null"' "$remote_pif_file_path" 2>/dev/null; then
+        log "Remote PIF file is bad, skip replace local PIF."
+        return 1
+    fi
+
+    example_pif_file_path="/data/adb/modules/playintegrityfix/example.pif.json"
+    if [ -e "$example_pif_file_path" ]; then
+        rm "$example_pif_file_path"
+        log "Delete example.pif.json"
+    fi
+
+    if diff "$remote_pif_file_path" "$PIF_FILE_PATH" > /dev/null 2>&1; then
+        log "The current and remote PIF are the same."
+    else
+        log "Replacing PIF with remote version."
+        cat "$remote_pif_file_path" > "$PIF_FILE_PATH"
+        handle_gms
+    fi
+
+    rm "$remote_pif_file_path"
+}
+
+handle_gms() {
+    processes="com.google.android.gms com.google.android.gms.unstable com.google.android.apps.walletnfcrel"
+
+    for process in $processes; do
+        pkill -f "$process" > /dev/null 2>&1
+        log "Stopped process: $process"
+    done
+
+    gms_cache_path="/data/data/com.google.android.gms/cache"
+    walletnfcrel_cache_path="/data/data/com.google.android.apps.walletnfcrel/cache"
+
+    if [ -d "$gms_cache_path" ] && [ "$(ls -A "$gms_cache_path")" ]; then
+        rm -r "$gms_cache_path"/* 2>/dev/null
+        log "Cleared cache: $gms_cache_path"
+    fi
+
+    if [ -d "$walletnfcrel_cache_path" ] && [ "$(ls -A "$walletnfcrel_cache_path")" ]; then
+        rm -r "$walletnfcrel_cache_path"/* 2>/dev/null
+        log "Cleared cache: $walletnfcrel_cache_path"
+    fi
+}
+
+# Wait for system to boot
+check_boot_interval=3
+until [ "$(getprop sys.boot_completed)" = "1" ]; do
+    sleep $check_boot_interval
+done
+
+# Wait for storage decryption, with timeout of 180 seconds
+i=0
+until [ -d "/storage/emulated/0/Android" ] || [ "$i" -ge 180 ]; do
+    sleep $check_boot_interval
+    i=$((i + check_boot_interval))
+done
+
+if ! [ -d "/data/adb/modules/playintegrityfix" ]; then
+    log "You need to install Play Integrity Fix (or Fork) module!"
+    log "Script is terminating due to the missing module"
+    exit 1
+fi
+
+# Get check interval from file with validation
+time_interval=$(cat "$MODPATH/minutes.txt" 2>/dev/null)
+if ! [ "$time_interval" -gt 0 ] 2>/dev/null; then
+    time_interval=25
+fi
+
+log "Script started. Checking network and PIF every $time_interval minutes."
+
+set_pif_file_path
+
+# Main loop: Check PIF file for updates by time_interval
+while true; do
+    log "" # Separator in log
+    log "Start new check."
+    if check_network_reachable; then
+        update_pif_if_needed
+        log "Check completed. Next check: $(date -d "@$(($(date +%s) + ($time_interval * 60)))" "+%Y-%m-%d_%H:%M:%S")"
+    fi
+    sleep "${time_interval}m"
+done
